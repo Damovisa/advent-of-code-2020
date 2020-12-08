1fc26dec2411d09022bba1d6f38aea24a7d83891
Damian Brady <dev@damianbrady.com.au>
2020-12-08 20:39:53 +1000

    Day 3 part 1

---
+++ day3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllLines("input.txt");
+
+            // Thoughts: You always go to the next line, so I should just be able to mod the width?   
+            var left = 0;
+            var patternWidth = input[0].Length;
+            var tree = 0;
+            for (int row = 1; row < input.Length; row++)
+            {
+                left+=3;
+                if (input[row][left%patternWidth] == '#') {
+                    tree++;
+                }
+            }
+            Console.WriteLine($"{tree} trees encountered");
+        }
+    }
+}
