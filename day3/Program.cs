7f1fb270c73e5d23f0364dd836fae64179bb37ff
Damian Brady <dev@damianbrady.com.au>
2020-12-08 20:47:33 +1000

    Day 3 part 2

--- day3/Program.cs
+++ day3/Program.cs
@@ -4,22 +4,36 @@ namespace day3
 {
     class Program
     {
+        static string[] input;
+
         static void Main(string[] args)
         {
-            var input = System.IO.File.ReadAllLines("input.txt");
+            input = System.IO.File.ReadAllLines("input.txt");
 
-            // Thoughts: You always go to the next line, so I should just be able to mod the width?   
-            var left = 0;
+            // Part 2 - extract a function
+            long r1d1 = GetTrees(1,1);
+            long r3d1 = GetTrees(3,1);
+            long r5d1 = GetTrees(5,1);
+            long r7d1 = GetTrees(7,1);
+            long r1d2 = GetTrees(1,2);
+
+            Console.WriteLine($"Multiplied: {r1d1*r3d1*r5d1*r7d1*r1d2}");
+        }
+
+        public static int GetTrees(int right, int down) {
+            Console.WriteLine($"Right {right}, Down {down}:");
             var patternWidth = input[0].Length;
+            var left = 0;
             var tree = 0;
-            for (int row = 1; row < input.Length; row++)
+            for (int row = down; row < input.Length; row+=down)
             {
-                left+=3;
+                left+=right;
                 if (input[row][left%patternWidth] == '#') {
                     tree++;
                 }
             }
-            Console.WriteLine($"{tree} trees encountered");
+            Console.WriteLine($"  {tree} trees encountered");
+            return tree;
         }
     }
 }
