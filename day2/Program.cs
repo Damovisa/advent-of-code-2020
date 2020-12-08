e66adb76d8633edcf7ca21e31bfae0e374bfd5ef
Damian Brady <dev@damianbrady.com.au>
2020-12-08 17:13:15 +1000

    Day 2 part 2

--- day2/Program.cs
+++ day2/Program.cs
@@ -8,32 +8,42 @@ namespace day2
     {
         static void Main(string[] args)
         {
-            // part 1
             var input = System.IO.File.ReadAllLines("input.txt");
             var records = input.Select(i =>
             {
                 var bits = i.Split(" ");
                 return new
                 {
-                    min = int.Parse(bits[0].Substring(0, bits[0].IndexOf("-"))),
-                    max = int.Parse(bits[0].Substring(bits[0].IndexOf("-") + 1)),
+                    first = int.Parse(bits[0].Substring(0, bits[0].IndexOf("-"))),
+                    second = int.Parse(bits[0].Substring(bits[0].IndexOf("-") + 1)),
                     letter = bits[1].Trim()[0].ToString(),
                     password = bits[2]
                 };
             }
             );
 
+            // part 1:
             var validCount = 0;
             foreach (var record in records) {
                 var instances = Regex.Matches(record.password, record.letter).Count;
-                if (instances >= record.min && instances <= record.max) {
+                if (instances >= record.first && instances <= record.second) {
                     Console.WriteLine($"VALID: {record}");
                     validCount++;
                 } else {
                     Console.WriteLine($"INVALID: {record}");
                 }
             }
-            Console.WriteLine($"Total valid: {validCount}");
+            Console.WriteLine($"Total valid under part 1: {validCount}");
+
+            // part 2:
+            validCount = 0;
+            foreach (var record in records) {
+                if (record.password[record.first-1] == record.letter[0] ^ record.password[record.second-1] == record.letter[0]) {
+                    Console.WriteLine($"VALID: {record}");
+                    validCount++;
+                }
+            }
+            Console.WriteLine($"Total valid under part 2: {validCount}");
         }
     }
 }
