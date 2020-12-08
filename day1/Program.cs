508aa0138a4e9f1fa1cd7df850bbf49123112aa3
Damian Brady <dev@damianbrady.com.au>
2020-12-08 16:30:40 +1000

    Day 1 complete

---
+++ day1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllLines("input.txt");
+            List<int> numbers = input.Select(i => int.Parse(i)).ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    for (int k = j + 1; k < numbers.Count; k++)
+                    {
+                        if (numbers[i] + numbers[j] + numbers[k] == 2020)
+                        {
+                            Console.WriteLine($"Numbers are {numbers[i]}, {numbers[j]}, and {numbers[k]}, which gives us {numbers[i] * numbers[j] * numbers[k]}");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
