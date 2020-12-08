619eeb6cd12d067b9f7b53a5f9abfa139688e815
Damian Brady <dev@damianbrady.com.au>
2020-12-08 17:08:27 +1000

    Day 2 part 1

---
+++ day2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // part 1
+            var input = System.IO.File.ReadAllLines("input.txt");
+            var records = input.Select(i =>
+            {
+                var bits = i.Split(" ");
+                return new
+                {
+                    min = int.Parse(bits[0].Substring(0, bits[0].IndexOf("-"))),
+                    max = int.Parse(bits[0].Substring(bits[0].IndexOf("-") + 1)),
+                    letter = bits[1].Trim()[0].ToString(),
+                    password = bits[2]
+                };
+            }
+            );
+
+            var validCount = 0;
+            foreach (var record in records) {
+                var instances = Regex.Matches(record.password, record.letter).Count;
+                if (instances >= record.min && instances <= record.max) {
+                    Console.WriteLine($"VALID: {record}");
+                    validCount++;
+                } else {
+                    Console.WriteLine($"INVALID: {record}");
+                }
+            }
+            Console.WriteLine($"Total valid: {validCount}");
+        }
+    }
+}
