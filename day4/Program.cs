a5dd2d51c2aeac967612e665ea54779facfff3c6
Damian Brady <dev@damianbrady.com.au>
2020-12-08 21:05:59 +1000

    Day 4 part 1

---
+++ day4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllText("input.txt");
+            var passports = input.Split("\r\n\r\n");
+
+            var requiredKeys = new[] {"byr","iyr","eyr","hgt","hcl","ecl","pid"}; //,"cid"};
+            var valid = 0;
+
+            foreach (var passport in passports) {
+                var keys = passport.Split(new[] {"\r\n", " "}, StringSplitOptions.None)
+                                   .Select(f => f.Substring(0,3));
+                if (requiredKeys.All(k => keys.Contains(k))) {
+                    valid++;
+                    Console.WriteLine("Passport valid");
+                }
+            }
+
+            Console.WriteLine($"Total: {valid}");
+        }
+    }
+}
