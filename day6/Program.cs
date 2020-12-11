42ee6503eb711aeff7fd0c2e9fccb7add332e830
Damian Brady <dev@damianbrady.com.au>
2020-12-11 16:31:26 +1000

    Day 6 part 1

---
+++ day6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var questions = new[] {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
+            var input = System.IO.File.ReadAllText("input.txt");
+            var groups = input.Split("\r\n\r\n");
+
+            int totalCount = 0;
+
+            foreach (var group in groups) {
+                var yeses = questions.Sum(q => group.Contains(q)?1:0);
+                Console.WriteLine(group);
+                Console.WriteLine($" -> {yeses}");
+                totalCount += yeses;
+            }
+
+            Console.WriteLine($"Total: {totalCount}");
+        }
+    }
+}
