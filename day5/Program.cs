1c18dc32e163bfba7d356d7515c5e5112bea4861
Damian Brady <dev@damianbrady.com.au>
2020-12-11 16:10:57 +1000

    Day 5 part 1

---
+++ day5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllLines("input.txt");
+
+            int maxRow = 0;
+
+            foreach (var line in input) {
+                // get value
+                var rowVal = GetValue(line.Substring(0,7));
+                var colVal = GetValue(line.Substring(7));
+                var seatId = rowVal * 8 + colVal;
+                Console.WriteLine($"'{input}' -> Row {rowVal}, Seat {colVal}, Seat ID {seatId}");
+                if (seatId > maxRow)
+                    maxRow = seatId;
+            }
+            Console.WriteLine($"Max Seat ID: {maxRow}");
+        }
+
+        static int GetValue(string encoded) {
+            var binString = encoded
+                .Replace("F","0").Replace("L", "0")
+                .Replace("B","1").Replace("R", "1");
+            return Convert.ToInt32(binString, 2);
+        }
+    }
+}
